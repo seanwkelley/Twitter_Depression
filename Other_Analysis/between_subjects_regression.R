d6dfb2084117d37670be5e6d4e540583c03d9ce8
Sean Kelley <seanwk21@gmail.com>
2020-05-29 14:43:20 -0700

    Change regression from SDS to depression zscore, add association with WC

--- Other_Analysis/between_subjects_regression.R
+++ Other_Analysis/between_subjects_regression.R
@@ -40,7 +40,6 @@ FYP_df <- read.csv('Data/Sentiments/all_tweets/VADER_ANEW_LIWC_complete.csv')
 colnames(FYP_df)[which(colnames(FYP_df) == 'Twitter_Handle')] = 'Id'
 
 participants <- read.csv('Data/Participant_Data/Twitter_Participants.csv')
-participants <- participants[which(is.na(participants$OCI_6) | participants$OCI_6 == 1),]
 
 #average sentiments over the past year 
 FYP_df_mean <- aggregate(. ~ Id , data = FYP_df, FUN = "mean")
@@ -55,80 +54,29 @@ FYP_df_mean$pro3 <- (FYP_df_mean$shehe + FYP_df_mean$they)/2
 #merge sentiments and participants data 
 FYP <- merge(participants,FYP_df_mean,by='Id')
 
-#reverse scored items: 2, 5, 6, 11, 12, 14, 16, 17, 18, 20  
-FYP$SDS_2 <- 5 - FYP$SDS_2
-FYP$SDS_5 <- 5 - FYP$SDS_5
-FYP$SDS_6 <- 5 - FYP$SDS_6
-FYP$SDS_11 <- 5 - FYP$SDS_11
-FYP$SDS_12 <- 5 - FYP$SDS_12
-FYP$SDS_14 <- 5 - FYP$SDS_14
-FYP$SDS_16 <- 5 - FYP$SDS_16
-FYP$SDS_17 <- 5 - FYP$SDS_17
-FYP$SDS_18 <- 5 - FYP$SDS_18
-FYP$SDS_20 <- 5 - FYP$SDS_20
-
-#SDS: Score system
-#Below 50 is considered normal range - 502 people (54.5%)
-#50-59 is mild depression - 259 people (28.1%)
-#60-69 is moderate depression - 138 people (14.9%)
-#above 70 is severe depression - 24 people (2.9%)
-
-FYP$SDS_Total <- rowSums(FYP %>% select(colnames(FYP)[grepl("SDS",colnames(FYP))]))
 FYP$Dep_ep_pastyear <- as.factor(FYP$Dep_ep_pastyear)
-FYP$Depression_Physician <- as.factor(FYP$Depression_Physician)
-
+FYP$Gender <- as.factor(FYP$Gender)
+FYP$Education <- as.factor(FYP$Education)
+FYP$Employment <- as.factor(FYP$Employment)
 ##################################################################
-#between-subjects regression of SDS total score and de choudhury variables 
+#between-subjects regression of depression z-score and de choudhury variables 
 
 #Betas (p-value) of de Choudhury variables 
-summary(glm(negemo ~ SDS_Total, family = "gaussian",data = FYP))
-summary(glm(posemo ~ SDS_Total, family = "gaussian",data = FYP))
-summary(glm(i ~ SDS_Total, family = "gaussian",data = FYP))
-summary(glm(we ~ SDS_Total, family = "gaussian",data = FYP))
-summary(glm(swear ~ SDS_Total, family = "gaussian",data = FYP))
-summary(glm(negate ~ SDS_Total, family = "gaussian",data = FYP))
-summary(glm(article ~ SDS_Total, family = "gaussian",data = FYP))
-
-
-#ANEW variables - dominance and arousal
-summary(glm(dominance ~ SDS_Total, family = "gaussian",data = FYP))
-summary(glm(arousal ~ SDS_Total, family = "gaussian",data = FYP))
+summary(glm(negemo ~ Depression_zscore, family = "gaussian",data = FYP))
+summary(glm(posemo ~ Depression_zscore, family = "gaussian",data = FYP))
+summary(glm(i ~ Depression_zscore, family = "gaussian",data = FYP))
+summary(glm(we ~ Depression_zscore, family = "gaussian",data = FYP))
+summary(glm(swear ~ Depression_zscore, family = "gaussian",data = FYP))
+summary(glm(negate ~ Depression_zscore, family = "gaussian",data = FYP))
+summary(glm(article ~ Depression_zscore, family = "gaussian",data = FYP))
+summary(glm(they ~ Depression_zscore, family = "gaussian",data = FYP))
+summary(glm(shehe ~ Depression_zscore, family = "gaussian",data = FYP))
+summary(glm(you ~ Depression_zscore, family = "gaussian",data = FYP))
 
 ##################################################################
-##################################################################
-#Figures 
 
-#Histogram of age
-ggplot(FYP, aes(x=Age)) + geom_histogram(bins=25,color="black", fill="white") + theme_bw() + 
-  ggtitle("Age Distribution") +
-  theme(
-    axis.title.x = element_text(size = 16),
-    axis.text.x = element_text(size = 14),
-    axis.title.y = element_text(size = 16))
-
-#histogram of SDS total score 
-ggplot(FYP, aes(x=SDS_Total)) + geom_histogram(bins=25,color="black", fill="white") + theme_bw() +
-  ggtitle("Depression Distribution") +
-  theme(
-    axis.title.x = element_text(size = 16),
-    axis.text.x = element_text(size = 14),
-    axis.title.y = element_text(size = 16))
-
-
-#boxplot of between-subject negemo for participants with/without a depressive episode in the past year
-ggplot(FYP, aes(x=Dep_ep_pastyear, y=negemo)) + 
-  geom_boxplot(notch = TRUE)  + theme_bw() +
-  theme(
-    axis.title.x = element_text(size = 16),
-    axis.text.x = element_text(size = 14),
-    axis.title.y = element_text(size = 16))
+summary(glm(WC ~ Dep_ep_pastyear + Age + Gender + Education + Employment, family = "gaussian",data = FYP))
 
 
 
-FYP %>% group_by(Dep_ep_pastyear) %>% summarise(mean(tweet)) %>%
-  mutate(
-    t_test = map2('0', '1', ~{t.test(.x$value, .y$value) %>% tidy()}),
-    '0' = map('0', nrow),
-    '1' = map('1', nrow)
-  ) %>% 
-  unnest()
+
